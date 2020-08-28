5d33ee18f93186148b126c1c18195d6abac5fdfa
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-08-28 23:03:21 +0600

    Update BellmanFord.cpp

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -1,74 +1,81 @@
-///...................................*****.................................................///
-///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
+vector < ll > adj[ siz ],cost[ siz ] ;
+ll n, m, x, y, z ;
+ll dist[ siz ] ;
 
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+void Input()
+{
+    scanf( "%lld %lld", &n, &m ) ;
+    for( int i = 0 ; i < m ; i ++ )
+    {
+        scanf( "%lld %lld %lld", &x, &y, &z ) ;
+        adj[ x ].pb( y ) ;
+        cost[ x ].pb( z ) ;
+    }
+}
 
-#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define          ll                     long long int
-#define          ld                     long double
-#define          ull                    unsigned long long int
-#define          pii                    pair < int, int>
-#define          pll                    pair < ll, ll>
-#define          MOD                    1000000007
-#define          vi                     vector<int>
-#define          vl                     vector<ll>
-#define          pb                     push_back
-#define          sc                     scanf
-#define          pf                     printf
-#define          scin(x)                scanf("%d",&(x))
-#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
-#define          scln(x)                scanf("%lld",&(x))
-#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
-#define          min3(a,b,c)            min(a,min(b,c))
-#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
-#define          max3(a,b,c)            max(a,max(b,c))
-#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
-#define          ms(a,b)                memset(a,b,sizeof(a))
-#define          mp                     make_pair
-#define          gcd(a, b)              __gcd(a,b)
-#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
-#define          input                  freopen("input.txt","rt", stdin)
-#define          output                 freopen("output.txt","wt", stdout)
-#define          PI                     acos(-1.0)
-#define          zero(a)                memset(a,0,sizeof a)
-#define          all(v)                 v.begin(),v.end()
-#define          Max(v)                 *max_element(all(v))
-#define          Min(v)                 *min_element(all(v))
-#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
-#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
-#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
-#define          no                     cout << "NO" << endl ;
-#define          yes                    cout << "YES" << endl ;
-///#define          endl                   '\n'
+bool Bellmanford( int s )
+{
+    for( int i = 1 ; i <= n ; i ++ ) dist[ i ] = INT_MAX ;
+    dist[ s ] = 0 ;
+    for( int i = 1 ; i <= n ; i ++ )
+    {
+        for( int j = 1 ; j <= n ; j ++  )
+        {
+            ll u = j ;
+            for( int k = 0 ; k < adj[ u ].size() ; k ++ )
+            {
+                ll v = adj[ u ][ k ] ;
+                ll w = cost[ u ][ k ] ;
+                if( dist[ u ] + w < dist[ v ] ) dist[ v ] = dist[ u ] + w ;
+            }
+        }
+    }
 
-///bool operator < ( const Node& p ) const{ return cost < p.cost ; }
-///priority_queue<int,vector<int>,greater<int> >pq;
-///string str = "abcdefghijklmnopqrstuvwxyz";
-///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-///string s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-///string s = std::bitset<64>(n).to_string();
-///---------------Order set-------------------
-///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-///#include <ext/pb_ds/assoc_container.hpp>
-///using namespace __gnu_pbds;
-///*os.find_by_order(index) os.order_of_key(value)
+    for( int j = 1 ; j <= n ; j ++  )
+    {
+        ll u = j ;
+        for( int k = 0 ; k < adj[ u ].size() ; k ++ )
+        {
+            ll v = adj[ u ][ k ] ;
+            ll w = cost[ u ][ k ] ;
+            if( dist[ u ] + w < dist[ v ] ) return false ;
+        }
+    }
+    return true ;
+}
 
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
+void Calculation()
+{
+    if( !Bellmanford( 1 ) ) pf( "Negative cycle detected\n" ) ;
+    else for( int i = 1 ; i <= n ; i ++ ) cout << dist[ i ] << endl ;
+}
+
+/*
+4 4
+1 2 4
+1 4 5
+4 3 3
+3 2 -10
+
+out - 0 -2 8 5
+
+4 5
+1 2 4
+1 4 5
+4 3 3
+3 2 -10
+2 4 5
+out - Negative cycle detected
+*/
+
+
+
+
+
+
+
+
+
 
 int dis[ 105 ] ;
 
