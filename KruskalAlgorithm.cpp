259aaaf969adfb25e34b6c3f341d44ce1fc3fc21
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-11-28 23:33:07 +0600

    Update KruskalAlgorithm.cpp

--- KruskalAlgorithm.cpp
+++ KruskalAlgorithm.cpp
@@ -1,75 +1,38 @@
-///...................................*****.................................................///
-///                  Mujahidul Islam ( mujahidulislam2923@gmail.com )                       ///
-///                  Department of Ict                                                      ///
-///                  Comilla University , Bangladesh.                                       ///
-///...................................*****.................................................///
+typedef pair< int, pair<int,int>> pii ;
 
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+int parent[ 105 ] ;
 
-#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define          ll                     long long int
-#define          ld                     long double
-#define          ull                    unsigned long long int
-#define          pii                    pair < int, int>
-#define          pll                    pair < ll, ll>
-#define          MOD                    1000000007
-#define          vi                     vector<int>
-#define          vl                     vector<ll>
-#define          pb                     push_back
-#define          sc                     scanf
-#define          pf                     printf
-#define          scin(x)                scanf("%d",&(x))
-#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
-#define          scln(x)                scanf("%lld",&(x))
-#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
-#define          min3(a,b,c)            min(a,min(b,c))
-#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
-#define          max3(a,b,c)            max(a,max(b,c))
-#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
-#define          ms(a,b)                memset(a,b,sizeof(a))
-#define          mp                     make_pair
-#define          gcd(a, b)              __gcd(a,b)
-#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
-#define          input                  freopen("input.txt","rt", stdin)
-#define          output                 freopen("output.txt","wt", stdout)
-#define          PI                     acos(-1.0)
-#define          zero(a)                memset(a,0,sizeof a)
-#define          all(v)                 v.begin(),v.end()
-#define          Max(v)                 *max_element(all(v))
-#define          Min(v)                 *min_element(all(v))
-#define          Upper(c,x)             (upper_bound(c.begin(),c.end(),x)-c.begin())
-#define          Lower(c,x)             (lower_bound(c.begin(),c.end(),x)-c.begin())
-#define          Unique(X)              (X).erase(unique(all(X)),(X).end())
-#define          no                     cout << "NO" << endl ;
-#define          yes                    cout << "YES" << endl ;
-///#define          endl                   '\n'
+vector < pii > vec ;
 
-///priority_queue<int,vector<int>,greater<int> >pq;
-///string str = "abcdefghijklmnopqrstuvwxyz";
-///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-///string s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
-///string s = std::bitset<64>(n).to_string();
-///---------------Order set-------------------
-///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-///#include <ext/pb_ds/assoc_container.hpp>
-///using namespace __gnu_pbds;
-///*os.find_by_order(index) os.order_of_key(value)
+int Find( int r )
+{
+    if( r == parent[ r ] ) return r ;
+    else return parent[ r ] = Find( parent[ r ] ) ;
+}
 
-///--------------Graph Moves--------------------------------------
-///const int fx[] = {+1,-1,+0,+0};
-///const int fy[] = {+0,+0,+1,-1};
-///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
-///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
-///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
-///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
-///---------------------------------------------------------------
-///sort( all( v ) ) ;
+int kruskal( int n )
+{
+    for( int i = 1 ; i <= n ; i ++ ) parent[ i ] = i ;
 
+    int cnt = 0, sum = 0 ;
+
+    for( int i = 0 ; i < vec.size() ; i ++ )
+    {
+        int u = Find( vec[ i ].second.first ) ;
+        int v = Find( vec[ i ].second.second ) ;
+        if( u != v )
+        {
+            parent[ u ] = v ;
+            sum += vec[ i ].first ;
+            cnt ++ ;
+            if( cnt == n - 1 ) break ;
+        }
+    }
+    return sum ;
+}
+
+
+-------------------------------------------------*****************-------------------------------------------------
 ll node, edge ;
 ll parent[ 100005 ] ;
 
@@ -93,10 +56,8 @@ vector< Node > vec ;
 
 ll Find( ll r )
 {
-    if( r == parent[ r ] )
-        return r ;
-    else
-        return parent[ r ] = Find( parent[ r ] ) ;
+    if( r == parent[ r ] ) return r ;
+    else return parent[ r ] = Find( parent[ r ] ) ;
 }
 
 ll kruskal()
@@ -116,8 +77,7 @@ ll kruskal()
             parent[ u ] = v ;
             cnt ++ ;
             weight += vec[ i ].cost ;
-            if( cnt == node - 1 )
-                break;
+            if( cnt == node - 1 ) break;
         }
     }
     return weight;
